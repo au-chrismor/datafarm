dc2d0c381ee0b679705fd4e0cf3d03b7580c00f3
Chris Moran <au-chrismor@users.noreply.github.com>
2023-10-30 15:12:13 +1100

    Initial cut for TankLevel

---
+++ src/Arduino/TankLevel/TankLevel.ino
@@ -0,0 +1,23 @@
+#include "config.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+#ifdef _DEBUG
+  Serial.println("Connecting");
+#endif
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+#ifdef _DEBUG
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+#endif
+}
+
+void loop() {
+}
