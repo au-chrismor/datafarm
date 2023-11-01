c78bb4d7d9ce77a75ed7c09660543052a6d5305f
Christopher Moran <chris.moran@emergent.com.au>
2023-11-01 11:33:02 +1100

    Turn data settings into params

--- src/Arduino/TankLevel/config.h
+++ src/Arduino/TankLevel/config.h
@@ -6,9 +6,13 @@
 
 #define _DEBUG
 #define TANK_LEVEL      36
-#define TANK_ADC_VALUE  0.001611328125
+#define TANK_ADC_VALUE  0.001611328125  // 2 * Vmax / ADC_Resolution)
 const char* WIFI_SSID = "WIFI";
 const char* WIFI_PASSWORD = "ShouldBeASecret";
 const char* SPLUNK_HOST = "splunk";
 const uint16_t SPLUNK_PORT = 8088;
+const char* INDEX_NAME = "datafarm";
+const char* HOST_NAME = "tanklevel";
+const char* SENSOR_TYPE = "tankdepth";
+const char* SOURCE_TYPE = "datafarm";
 const char* SPLUNK_TOKEN = "Authorization: Splunk 01234567-89ab-def0-1234-56789abcdef0";
