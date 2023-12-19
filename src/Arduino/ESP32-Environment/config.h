42673ba75d42afabc7fa93b1094075033d2e4297
Christopher Moran <chris.moran@emergent.com.au>
2023-12-19 16:31:56 +1100

    Add ESP32 Environment Package

---
+++ src/Arduino/ESP32-Environment/config.h
@@ -0,0 +1,12 @@
+
+#ifndef STASSID
+#define STASSID "SSID"
+#define STAPSK "PASSWORD"
+#endif
+
+const char* host = "splunk";
+const uint16_t port = 8088;
+
+#define DEPTH_SENSOR A0
+#define DEPTH_CALC_VAL 0.001611328
+#define SPLUNK_TOKEN "Authorization: Splunk uuid"
