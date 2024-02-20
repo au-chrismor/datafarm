d99b9847a073df1f915b6b8b6bbf9ae4cc7a4f1e
Christopher Moran <chris.moran@emergent.com.au>
2024-02-20 17:30:44 +1100

    Adding DC Power monitor

---
+++ src/Arduino/ESP32-Power/config.h
@@ -0,0 +1,12 @@
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK ""
+#endif
+
+const char* host = "splunk";
+const uint16_t port = 8088;
+
+#define VOLT_PIN  34
+#define AMP_PIN  35
+#define SPLUNK_TOKEN "Authorization: Splunk ..."
