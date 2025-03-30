de5ef277a606610151516be032351b0159e19fb0
Christopher Moran <chris.moran@emergent.com.au>
2025-03-30 18:12:08 +1100

    Adding Thermistor Reader

---
+++ src/Arduino/ESP32-Thermistor/config.h
@@ -0,0 +1,13 @@
+
+#ifndef STASSID
+#define STASSID "SSID"
+#define STAPSK "<<ChangeMe>>"
+#endif
+
+const char* host = "splunk";
+const uint16_t port = 8088;
+
+#define NTC_PIN   34
+#define NTC_RES   10000
+#define NTC_BETA   3950
+#define SPLUNK_TOKEN "Authorization: etc"
