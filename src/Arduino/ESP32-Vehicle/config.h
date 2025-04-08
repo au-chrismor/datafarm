8de7967a92a0a4dea081baa31069719325d05e1d
Christopher Moran <chris.moran@emergent.com.au>
2025-04-08 13:08:11 +1000

    Adding GPS Location Tracking

---
+++ src/Arduino/ESP32-Vehicle/config.h
@@ -0,0 +1,16 @@
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK ""
+#endif
+
+const char* deviceName = "vehicle1";
+const char* sensorType = "location";
+
+const char* host = "splunk";
+const uint16_t port = 8088;
+
+#define SPLUNK_TOKEN "Authorization: Splunk "
+#define GPS_BAUD 9600
+#define RXD2 17
+#define TXD2 16
