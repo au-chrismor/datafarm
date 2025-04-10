de3c756be568ad76d3ae6eb19d923852e2adcbb5
Christopher Moran <chris.moran@emergent.com.au>
2025-04-11 09:52:53 +1000

    Adding vehicle (GPS+Temperature)

--- src/Arduino/ESP32-Vehicle/ESP32-Vehicle.ino
+++ src/Arduino/ESP32-Vehicle/ESP32-Vehicle.ino
@@ -2,12 +2,20 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <TinyGPS.h>
+#include <thermistor.h>
+
 
 WiFiMulti WiFiMulti;
 
 HardwareSerial gpsSerial(2);
 TinyGPS gps;
 
+THERMISTOR thermistor(
+  NTC_PIN,
+  NTC_RES,
+  NTC_BETA,
+  NTC_RES
+);
 
 void setup() {
   Serial.begin(115200);
@@ -39,6 +47,8 @@ void loop() {
   float speed = 0.0;
   unsigned long satellites = 0;
   unsigned long age = 0;
+  uint16_t  temperature = -2740;
+  
 
   while(gpsSerial.available() > 0)
     {gps.encode(gpsSerial.read());
@@ -49,6 +59,8 @@ void loop() {
   speed = gps.f_speed_kmph();
   altitude = gps.f_altitude();
   heading = gps.f_course();
+
+  temperature = thermistor.read();
 
   String data = "{\"host\": \"";
   data += deviceName;
@@ -65,6 +77,7 @@ void loop() {
   data += "\"heading\": \"" + String(heading) + "\",";
   data += "\"speed\": \"" + String(speed) + "\",";
   data += "\"satellites\": \"" + String(satellites) + "\",";
+  data += "\"temperature\": \"" + String(temperature/10) + "\",";
   data += "\"battery\": \"-1\"";
   data += "}}";
   Serial.println(data);
