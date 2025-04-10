b7de2fe4f1f1f9bf47a24acb384530c7cd476666
Christopher Moran <chris.moran@emergent.com.au>
2025-04-10 11:11:27 +1000

    Device names and types now in config

--- src/Arduino/ESP32-Temperature/config.h
+++ src/Arduino/ESP32-Temperature/config.h
@@ -7,5 +7,8 @@
 const char* host = "splunk";
 const uint16_t port = 8088;
 
+const char* deviceName = "temperature1";
+const char* sensorType = "temperature";
+
 #define ONEWIRE_PIN  4
 #define SPLUNK_TOKEN "Authorization: Splunk uuid"
